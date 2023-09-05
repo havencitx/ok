245847013caadfcdcef1279b98f7f6ea530b0da8
havencitx <144144761+havencitx@users.noreply.github.com>
2023-09-05 10:56:59 -0400

    Create number1.cpp

---
+++ number1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Displaying name, address, city, state, and zip code
+    std::cout << "Name: Albert Clerge" << std::endl;
+    std::cout << "Street Address: 123 Sherborne Ln" << std::endl;
+    std::cout << "City, State, Zip Code: Kissimmee, Florida, 34758" << std::endl;
+
+    return 0;
+}
